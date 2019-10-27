61f557d0c4e2bf2b1bf198fc86c72b3c35719fa6
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-10-27 22:21:26 +0300

    solution to exercise 1: speed converter

---
+++ exercise-1-speed-converter/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        SpeedConverter.printConversion(-1);
+        SpeedConverter.printConversion(0);
+        SpeedConverter.printConversion(1.5);
+        SpeedConverter.printConversion(12.5);
+    }
+}
---
+++ exercise-1-speed-converter/src/SpeedConverter.java
@@ -0,0 +1,20 @@
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+        }
+    }
+}
