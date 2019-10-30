57d5b912c1cb43f94c98a5865fda75c8d29748f7
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-10-31 00:36:46 +0300

    solution to challenge 5: switch statement

---
+++ challenge-5-switch-statement/src/SwitchStatement.java
@@ -0,0 +1,42 @@
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(1);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(3);
+        printDayOfTheWeek(4);
+        printDayOfTheWeek(5);
+        printDayOfTheWeek(6);
+        printDayOfTheWeek(7);
+        printDayOfTheWeek(8);
+    }
+    
+    private static void printDayOfTheWeek(int day) {
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+}
