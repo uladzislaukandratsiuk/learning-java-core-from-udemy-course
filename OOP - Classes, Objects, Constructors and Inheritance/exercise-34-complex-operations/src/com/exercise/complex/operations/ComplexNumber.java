4c03b7a9754194536631a75215d35debc3ce726a
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-11-12 17:01:09 +0300

    solution to exercise 34: complex operations

---
+++ OOP - Classes, Objects, Constructors and Inheritance/exercise-34-complex-operations/src/com/exercise/complex/operations/ComplexNumber.java
@@ -0,0 +1,40 @@
+package com.exercise.complex.operations;
+
+public class ComplexNumber {
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber number) {
+        this.real += number.real;
+        this.imaginary += number.imaginary;
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber number) {
+        this.real -= number.real;
+        this.imaginary -= number.imaginary;
+    }
+}
