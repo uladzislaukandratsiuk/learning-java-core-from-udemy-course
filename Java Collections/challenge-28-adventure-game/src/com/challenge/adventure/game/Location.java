685cc153da2db4cd5680eb75720831578fe2a3e4
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-12-05 18:32:06 +0300

    solution to challenge 28: adventure game (make immutable class)

--- Java Collections/challenge-28-adventure-game/src/com/challenge/adventure/game/Location.java
+++ Java Collections/challenge-28-adventure-game/src/com/challenge/adventure/game/Location.java
@@ -9,15 +9,11 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>();
+        this.exits = exits != null ? new HashMap<>(exits) : new HashMap<>();
         this.exits.put("Q", 0);
-    }
-
-    public void addExit(String direction, int location) {
-        this.exits.put(direction, location);
     }
 
     public int getLocationID() {
@@ -29,6 +25,6 @@ public class Location {
     }
 
     public Map<String, Integer> getExits() {
-        return new HashMap<>(exits);
+        return new HashMap<>(this.exits);
     }
 }
--- Java Collections/challenge-28-adventure-game/src/com/challenge/adventure/game/Main.java
+++ Java Collections/challenge-28-adventure-game/src/com/challenge/adventure/game/Main.java
@@ -10,27 +10,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
+        locations.put(0, new Location(0,
+                "You are sitting in front of a computer learning Java", null));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
+        Map<String, Integer> tempExits = new HashMap<>();
+        tempExits.put("W", 2);
+        tempExits.put("E", 3);
+        tempExits.put("S", 4);
+        tempExits.put("N", 5);
+        locations.put(1, new Location(1,
+                "You are standing at the end of a road before a small brick building", tempExits));
 
-        locations.get(2).addExit("N", 5);
+        tempExits = new HashMap<>();
+        tempExits.put("N", 5);
+        locations.put(2, new Location(2,
+                "You are at the top of a hill", tempExits));
 
-        locations.get(3).addExit("W", 1);
+        tempExits = new HashMap<>();
+        tempExits.put("W", 1);
+        locations.put(3, new Location(3,
+                "You are inside a building, a well house for a small spring", tempExits));
 
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
+        tempExits = new HashMap<>();
+        tempExits.put("N", 1);
+        tempExits.put("W", 2);
+        locations.put(4, new Location(4,
+                "You are in a valley beside a stream", tempExits));
 
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
+        tempExits = new HashMap<>();
+        tempExits.put("S", 1);
+        tempExits.put("W", 2);
+        locations.put(5, new Location(5,
+                "You are in the forest", tempExits));
 
         int loc = 1;
         while (true) {
