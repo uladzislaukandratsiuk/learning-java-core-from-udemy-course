f2470e19f877f9688bdc84dbf859544236d3521e
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-12-11 20:52:16 +0300

    solution to challenge 31: input & output (part 1)

---
+++ Basic Input & Output including java.util/challenge-31-input-output/src/com/challenge/input/output/Locations.java
@@ -0,0 +1,106 @@
+package com.challenge.input.output;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Locations implements Map<Integer, Location> {
+
+    public static Map<Integer, Location> locations = new HashMap<>();
+
+    static {
+        locations.put(0, new Location(0,
+                "You are sitting in front of a computer learning Java", null));
+
+        Map<String, Integer> tempExits = new HashMap<>();
+        tempExits.put("W", 2);
+        tempExits.put("E", 3);
+        tempExits.put("S", 4);
+        tempExits.put("N", 5);
+        locations.put(1, new Location(1,
+                "You are standing at the end of a road before a small brick building", tempExits));
+
+        tempExits = new HashMap<>();
+        tempExits.put("N", 5);
+        locations.put(2, new Location(2,
+                "You are at the top of a hill", tempExits));
+
+        tempExits = new HashMap<>();
+        tempExits.put("W", 1);
+        locations.put(3, new Location(3,
+                "You are inside a building, a well house for a small spring", tempExits));
+
+        tempExits = new HashMap<>();
+        tempExits.put("N", 1);
+        tempExits.put("W", 2);
+        locations.put(4, new Location(4,
+                "You are in a valley beside a stream", tempExits));
+
+        tempExits = new HashMap<>();
+        tempExits.put("S", 1);
+        tempExits.put("W", 2);
+        locations.put(5, new Location(5,
+                "You are in the forest", tempExits));
+    }
+
+    @Override
+    public int size() {
+        return locations.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return locations.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return locations.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return locations.containsValue(value);
+    }
+
+    @Override
+    public Location get(Object key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public Location put(Integer key, Location value) {
+        return locations.put(key, value);
+    }
+
+    @Override
+    public Location remove(Object key) {
+        return locations.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Location> m) {
+
+    }
+
+    @Override
+    public void clear() {
+        locations.clear();
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return locations.keySet();
+    }
+
+    @Override
+    public Collection<Location> values() {
+        return locations.values();
+    }
+
+    @Override
+    public Set<Entry<Integer, Location>> entrySet() {
+        return locations.entrySet();
+    }
+}
