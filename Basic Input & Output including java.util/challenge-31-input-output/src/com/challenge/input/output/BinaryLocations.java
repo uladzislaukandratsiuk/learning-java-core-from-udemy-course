966855cb0d46c2de5045f28e76f26aa9f0feaecd
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-12-14 19:01:00 +0300

    solution to challenge 31: input & output (part 5)

---
+++ Basic Input & Output including java.util/challenge-31-input-output/src/com/challenge/input/output/BinaryLocations.java
@@ -0,0 +1,121 @@
+package com.challenge.input.output;
+
+import java.io.*;
+import java.util.*;
+
+public class BinaryLocations implements Map<Integer, Location> {
+
+    public static Map<Integer, Location> locations = new LinkedHashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        try (DataOutputStream locationsFile =
+                     new DataOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")))) {
+            for (Location location : locations.values()) {
+                locationsFile.writeInt(location.getLocationID());
+                locationsFile.writeUTF(location.getDescription());
+                System.out.println("Writing location " + location.getLocationID() + " : " + location.getDescription());
+                System.out.println("Writing " + (location.getExits().size() - 1) + " exits.");
+                locationsFile.writeInt(location.getExits().size() - 1);
+                for (String direction : location.getExits().keySet()) {
+                    if (!direction.equalsIgnoreCase("Q")) {
+                        System.out.println("\t\t" + direction + "," + location.getExits().get(direction));
+                        locationsFile.writeUTF(direction);
+                        locationsFile.writeInt(location.getExits().get(direction));
+                    }
+                }
+            }
+        }
+    }
+
+    static {
+        try (BufferedReader locationsFile = new BufferedReader(new FileReader("locations_big.txt"))) {
+            String input;
+            while ((input = locationsFile.readLine()) != null) {
+                String[] data = input.split(",");
+                int loc = Integer.parseInt(data[0]);
+                String description = data[1];
+                System.out.println("Imported loc: " + loc + ": " + description);
+                Map<String, Integer> tempExit = new HashMap<>();
+                locations.put(loc, new Location(loc, description, tempExit));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader directionsFile = new BufferedReader(new FileReader("directions_big.txt"))) {
+            String input;
+            while ((input = directionsFile.readLine()) != null) {
+                String[] data = input.split(",");
+                int loc = Integer.parseInt(data[0]);
+                String direction = data[1];
+                int destination = Integer.parseInt(data[2]);
+                System.out.println(loc + ": " + direction + ": " + destination);
+                Location location = locations.get(loc);
+                location.addExit(direction, destination);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int size() {
+        return locations.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return locations.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return locations.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return locations.containsValue(value);
+    }
+
+    @Override
+    public Location get(Object key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public Location put(Integer key, Location value) {
+        return locations.put(key, value);
+    }
+
+    @Override
+    public Location remove(Object key) {
+        return locations.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Location> m) {
+
+    }
+
+    @Override
+    public void clear() {
+        locations.clear();
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return locations.keySet();
+    }
+
+    @Override
+    public Collection<Location> values() {
+        return locations.values();
+    }
+
+    @Override
+    public Set<Map.Entry<Integer, Location>> entrySet() {
+        return locations.entrySet();
+    }
+}
+
