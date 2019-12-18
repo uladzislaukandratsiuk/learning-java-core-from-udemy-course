29a28d7dd98f6c20080299f3b1ae0b19a993bcc3
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-12-18 19:17:27 +0300

    solution to challenge 32: input & output nio (part 2)

---
+++ Input & Output NIO/challenge-32-input-output-nio/src/com/challenge/nio/ObjectNIOLocations.java
@@ -0,0 +1,123 @@
+package com.challenge.nio;
+
+import java.io.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class ObjectNIOLocations implements Map<Integer, Location> {
+
+    private static final String BIG_LOCATIONS_PATH = "Input & Output NIO/locations_big.txt";
+    private static final String BIG_DIRECTIONS_PATH = "Input & Output NIO/directions_big.txt";
+    private static final String OBJECT_LOCATIONS_PATH = "Input & Output NIO/locations.dat";
+
+
+    public static Map<Integer, Location> locations = new LinkedHashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        Path locPath = FileSystems.getDefault().getPath(OBJECT_LOCATIONS_PATH);
+        try (ObjectOutputStream locFile =
+                     new ObjectOutputStream(new BufferedOutputStream(Files.newOutputStream(locPath)))) {
+            for (Location location : locations.values()) {
+                locFile.writeObject(location);
+            }
+        }
+    }
+
+    static {
+        Path locPath = FileSystems.getDefault().getPath(BIG_LOCATIONS_PATH);
+        Path dirPath = FileSystems.getDefault().getPath(BIG_DIRECTIONS_PATH);
+
+        try (Scanner scanner = new Scanner(Files.newBufferedReader(locPath))) {
+            scanner.useDelimiter(",");
+            while (scanner.hasNextLine()) {
+                int loc = scanner.nextInt();
+                scanner.skip(scanner.delimiter());
+                String description = scanner.nextLine();
+                System.out.println("Imported loc: " + loc + ": " + description);
+                locations.put(loc, new Location(loc, description, null));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader dirFile = Files.newBufferedReader(dirPath)) {
+            String input;
+
+            while ((input = dirFile.readLine()) != null) {
+                String[] data = input.split(",");
+                int loc = Integer.parseInt(data[0]);
+                String direction = data[1];
+                int destination = Integer.parseInt(data[2]);
+                System.out.println(loc + ": " + direction + ": " + destination);
+                Location location = locations.get(loc);
+                location.addExit(direction, destination);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    @Override
+    public int size() {
+        return locations.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return locations.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return locations.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return locations.containsValue(value);
+    }
+
+    @Override
+    public Location get(Object key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public Location put(Integer key, Location value) {
+        return locations.put(key, value);
+    }
+
+    @Override
+    public Location remove(Object key) {
+        return locations.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Location> m) {
+
+    }
+
+    @Override
+    public void clear() {
+        locations.clear();
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return locations.keySet();
+    }
+
+    @Override
+    public Collection<Location> values() {
+        return locations.values();
+    }
+
+    @Override
+    public Set<Map.Entry<Integer, Location>> entrySet() {
+        return locations.entrySet();
+    }
+}
