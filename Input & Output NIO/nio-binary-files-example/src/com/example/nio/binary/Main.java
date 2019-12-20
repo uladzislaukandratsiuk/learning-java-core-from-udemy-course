b2e3eee9d09d42823193d99cc406484cdf545143
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-12-20 19:43:51 +0300

    nio binary reading example

--- Input & Output NIO/nio-binary-files-example/src/com/example/nio/binary/Main.java
+++ Input & Output NIO/nio-binary-files-example/src/com/example/nio/binary/Main.java
@@ -2,6 +2,7 @@ package com.example.nio.binary;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -30,6 +31,45 @@ public class Main {
             intBuffer.flip();
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes written was: " + numBytes);
+
+            RandomAccessFile ra = new RandomAccessFile(FILE_PATH, "rwd");
+            FileChannel channel = ra.getChannel();
+
+            outputBytes[0] = 'a';
+            outputBytes[1] = 'b';
+
+            long numBytesReadWithoutFlip = channel.read(buffer);
+            if(buffer.hasArray()) {
+                System.out.println("byte buffer = " + new String(buffer.array()));
+            }
+
+            buffer.flip();
+            long numBytesRead = channel.read(buffer);
+            if(buffer.hasArray()) {
+                System.out.println("byte buffer = " + new String(buffer.array()));
+            }
+
+            // Absolute read
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+
+
+            // Relative read
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//            intBuffer.flip();
+//            System.out.println(intBuffer.getInt());
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//            intBuffer.flip();
+//            System.out.println(intBuffer.getInt());
+
+            channel.close();
+            ra.close();
 
         } catch(IOException e) {
             e.printStackTrace();
