327ad18b4aae92f661fd5f5dd3d166b46b7e1552
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-12-25 21:25:51 +0300

    concurrency interrupt and join example

--- Concurrency in Java/threads-example/src/com/examples/threads/Main.java
+++ Concurrency in Java/threads-example/src/com/examples/threads/Main.java
@@ -17,10 +17,17 @@ public class Main {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()!");
+                try {
+                    myThread.join();
+                    System.out.println(ANSI_RED + "MyThread terminated, or timed out, so I'm running again");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait. I was interrupted");
+                }
             }
         });
 
         myRunnable.start();
+        myThread.interrupt();
 
         System.out.println(ANSI_GREEN + "Hello again from the main thread!");
     }
--- Concurrency in Java/threads-example/src/com/examples/threads/MyThread.java
+++ Concurrency in Java/threads-example/src/com/examples/threads/MyThread.java
@@ -12,6 +12,7 @@ public class MyThread extends Thread {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             System.out.println(ANSI_BLUE +  "Another thread woke me up");
+            return;
         }
 
         System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake");
