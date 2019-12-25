c05dcd2c4a5115b6841ee53257ccc4c761c0e7d3
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-12-25 22:00:50 +0300

    concurrency Thread instance variable example

---
+++ Concurrency in Java/threads-variables-example/src/com/examples/threads/variables/Main.java
@@ -0,0 +1,55 @@
+package com.examples.threads.variables;
+
+import com.examples.threads.ThreadColor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Countdown countdown = new Countdown();
+
+        CountdownThread t1 = new CountdownThread(countdown);
+        t1.setName("Thread 1");
+        CountdownThread t2 = new CountdownThread(countdown);
+        t2.setName("Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class Countdown {
+
+    private int i;
+
+    public void doCountdown() {
+        String color;
+
+        switch(Thread.currentThread().getName()) {
+            case "Thread 1":
+                color = ThreadColor.ANSI_GREEN;
+                break;
+            case "Thread 2":
+                color = ThreadColor.ANSI_BLUE;
+                break;
+            default:
+                color = ThreadColor.ANSI_RED;
+        }
+
+        for(i = 0; i < 6; i++) {
+            System.out.println(color + Thread.currentThread().getName() + ": i = " + i);
+        }
+    }
+}
+
+class CountdownThread extends Thread {
+    private Countdown threadCountdown;
+
+    public CountdownThread(Countdown countdown) {
+        threadCountdown = countdown;
+    }
+
+    public void run() {
+        threadCountdown.doCountdown();
+    }
+}
---
+++ Concurrency in Java/threads-variables-example/src/com/examples/threads/variables/ThreadColor.java
@@ -0,0 +1,7 @@
+package com.examples.threads.variables;
+
+public class ThreadColor {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+}
