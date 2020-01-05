777f20550e8daab448fed8a0505e164a361c83ec
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-01-05 23:31:59 +0300

    solution to challenge 33: synchronize code with tryLock() method (part 4)

--- Concurrency in Java/challenge-33-concurrency/src/com/challenge/java/concrurrency/BankAccount.java
+++ Concurrency in Java/challenge-33-concurrency/src/com/challenge/java/concrurrency/BankAccount.java
@@ -1,5 +1,6 @@
 package com.challenge.java.concrurrency;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -25,21 +26,35 @@ class BankAccount {
     }
 
     public synchronized void deposit(double amount) {
-        lock.lock();
         try {
-            balance += amount;
-        } finally {
-            lock.unlock();
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance += amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get lock");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         showBalance();
     }
 
     public void withdraw(double amount) {
-        lock.lock();
         try {
-            balance -= amount;
-        } finally {
-            lock.unlock();
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance -= amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get lock");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         showBalance();
     }
