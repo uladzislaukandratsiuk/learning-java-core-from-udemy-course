1e0b47154d9e175624c5b24c0f26c2273121296c
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-01-11 22:21:17 +0300

    stream operations example (part 2)

--- Lambda Expressions/stream-example/src/com/example/stream/Main.java
+++ Lambda Expressions/stream-example/src/com/example/stream/Main.java
@@ -1,6 +1,5 @@
 package com.example.stream;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,15 +12,11 @@ public class Main {
                 "B12", "b61", "B35", "B26", "B29", "b56", "B42",
                 "C45", "C62", "C52", "C16", "C21", "C76");
 
-        List<String> bSeats = new ArrayList<>();
-
-        theatreSeats.forEach(seat -> {
-           if (seat.toUpperCase().startsWith("B")) {
-                bSeats.add(seat);
-           }
-        });
-
-        bSeats.sort((String s1, String s2) -> s1.compareTo(s2));
-        bSeats.forEach((String s) -> System.out.println(s));
+       theatreSeats
+               .stream()
+               .map(String::toUpperCase)
+               .filter(s -> s.startsWith("B"))
+               .sorted()
+               .forEach(System.out::println);
     }
 }
