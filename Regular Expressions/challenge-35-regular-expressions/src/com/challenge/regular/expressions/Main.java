fd126d288c20315bc6572e0714a54d503577795c
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-01-16 23:17:36 +0300

    solution to challenge 35: regular expressions (part 2)

--- Regular Expressions/challenge-35-regular-expressions/src/com/challenge/regular/expressions/Main.java
+++ Regular Expressions/challenge-35-regular-expressions/src/com/challenge/regular/expressions/Main.java
@@ -9,10 +9,10 @@ public class Main {
     public static void main(String[] args) {
         String fistChallenge = "I want a bike.";
 
-        System.out.println(fistChallenge.matches("I want a bike."));
+        System.out.println(fistChallenge.matches("I want a bike\\."));
 
         String secondChallenge = "I want a ball.";
-        String regExp = "I want a b\\w+.";
+        String regExp = "I want a b\\w+\\.";
 
         System.out.println(fistChallenge.matches(regExp));
         System.out.println(secondChallenge.matches(regExp));
@@ -35,5 +35,50 @@ public class Main {
         String fifthChallenge = "nvsdj.643";
         System.out.println(fifthChallenge.matches("^\\w+\\.\\d+$"));
         System.out.println(fifthChallenge.matches("([A-z][a-z]+\\.[0-9]+)"));
+
+        String sixthChallenge = "nvsdj.643sf.41fng.dsf52dsv.142";
+        System.out.println(sixthChallenge.replaceAll("\\D"," ")
+                .replaceAll("^\\s+", "")
+                .replaceAll("\\s+", " "));
+
+        Pattern sixthPattern = Pattern.compile("(\\d+)");
+        Matcher sixthMatcher = sixthPattern.matcher(sixthChallenge);
+
+        while (sixthMatcher.find()) {
+            System.out.print(sixthMatcher.group() + " ");
+        }
+
+        System.out.println();
+
+        String seventhChallenge = "nvsdj.643\tsf.41\tfng.ds52dsv.142\n";
+
+        Pattern seventhPattern = Pattern.compile("[\\w]+\\.([?\\w]*\\d+[?\\w]*\\.)\\s");
+        Matcher seventhMatcher = seventhPattern.matcher(seventhChallenge);
+
+        while (seventhMatcher.find()) {
+            System.out.print(seventhMatcher.group(1) + " ");
+        }
+
+        String eighthChallenge = "nvsdj.643\tsf.41\tfng.ds52dsv.142\n";
+
+        Pattern eighthPattern = Pattern.compile("\\d+");
+        Matcher eighthMatcher = eighthPattern.matcher(eighthChallenge);
+
+        int count = 0;
+        while (eighthMatcher.find()) {
+            count++;
+            System.out.print("count = " + count + " number = " + eighthMatcher.group()
+                    + " start = " + eighthMatcher.start()
+                    + " end = " + eighthMatcher.end() + "\n");
+        }
+
+        String ninthChallenge = "{0, 2}, {0, 5}, {1, 3}, {2, 4}, {x, y], {z, c}, {13, 2}";
+
+        Pattern ninthPattern = Pattern.compile("\\{(\\d+, \\d+)}");
+        Matcher ninthMatcher = ninthPattern.matcher(ninthChallenge);
+
+        while (ninthMatcher.find()) {
+            System.out.print(ninthMatcher.group(1) + "\n");
+        }
     }
 }
