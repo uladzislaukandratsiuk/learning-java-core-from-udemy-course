325e34ff0ff397b632316a0a1e2fad5ce566dee4
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-01-18 21:52:07 +0300

    unit testing with junit 5 Asserts example (part 2)

--- Unit Testing/unit-testing-with-junit-example/src/com/example/unit/testing/BankAccount.java
+++ Unit Testing/unit-testing-with-junit-example/src/com/example/unit/testing/BankAccount.java
@@ -6,10 +6,16 @@ public class BankAccount {
     private String lastName;
     private double balance;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 2;
+
+    private int accountType;
+
+    public BankAccount(String firstName, String lastName, double balance, int accountType) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.accountType = accountType;
     }
 
     // The branch argument is true if the customer is performing the transaction
@@ -30,6 +36,10 @@ public class BankAccount {
 
     public double getBalance() {
         return balance;
+    }
+
+    public boolean isChecking() {
+        return accountType == CHECKING;
     }
 
     // More methods that use firstName, lastName, and perform other functions
--- Unit Testing/unit-testing-with-junit-example/test/com/example/unit/testing/BankAccountTest.java
+++ Unit Testing/unit-testing-with-junit-example/test/com/example/unit/testing/BankAccountTest.java
@@ -2,22 +2,46 @@ package com.example.unit.testing;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest {
 
     @Test
     void deposit() {
-        fail("a failing test");
+        BankAccount bankAccount =
+                new BankAccount("Vlad", "Kondratuk", 800.00, BankAccount.CHECKING);
+        double balance = bankAccount.deposit(350.00, true);
+        assertEquals(1150.00, balance, 0);
     }
 
     @Test
     void withdraw() {
-        fail("a failing test");
+        BankAccount bankAccount =
+                new BankAccount("Vlad", "Kondratuk", 1150.00, BankAccount.CHECKING);
+        double balance = bankAccount.withdraw(350.00, true);
+        assertEquals(800.00, balance, 0);
     }
 
     @Test
-    void getBalance() {
-        fail("a failing test");
+    void getBalance_deposit() {
+        BankAccount bankAccount =
+                new BankAccount("Vlad", "Kondratuk", 800.00, BankAccount.CHECKING);
+        bankAccount.deposit(350.00, true);
+        assertEquals(1150.00, bankAccount.getBalance(), 0);
+    }
+
+    @Test
+    void getBalance_withdraw() {
+        BankAccount bankAccount =
+                new BankAccount("Vlad", "Kondratuk", 1150.00, BankAccount.CHECKING);
+        bankAccount.withdraw(350.00, true);
+        assertEquals(800.00, bankAccount.getBalance(), 0);
+    }
+
+    @Test
+    void isChecking() {
+        BankAccount bankAccount =
+                new BankAccount("Vlad", "Kondratuk", 1150.00, BankAccount.CHECKING);
+        assertTrue(bankAccount.isChecking());
     }
 }
