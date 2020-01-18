c233e81c57a10b810429f63a85091d3644d0ab08
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-01-18 21:11:00 +0300

    unit testing with junit 5 example (part 1)

---
+++ Unit Testing/unit-testing-with-junit-example/test/com/example/unit/testing/BankAccountTest.java
@@ -0,0 +1,23 @@
+package com.example.unit.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class BankAccountTest {
+
+    @Test
+    void deposit() {
+        fail("a failing test");
+    }
+
+    @Test
+    void withdraw() {
+        fail("a failing test");
+    }
+
+    @Test
+    void getBalance() {
+        fail("a failing test");
+    }
+}
