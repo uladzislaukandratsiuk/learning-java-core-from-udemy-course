1a7220636f06e4d93484ce50a0711db2ea32fff3
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-01-19 22:54:57 +0300

    solution to challenge 36: unit testing with junit 5 (part 1)

---
+++ Unit Testing/challenge-36-unit-testing-with-junit5/test/com/challenge/unit/testing/UtilitiesTest.java
@@ -0,0 +1,28 @@
+package com.challenge.unit.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class UtilitiesTest {
+
+    @Test
+    void everyNthChar() {
+        fail("Test not implemented");
+    }
+
+    @Test
+    void removePairs() {
+        fail("Test not implemented");
+    }
+
+    @Test
+    void converter() {
+        fail("Test not implemented");
+    }
+
+    @Test
+    void nullIfOddLength() {
+        fail("Test not implemented");
+    }
+}
