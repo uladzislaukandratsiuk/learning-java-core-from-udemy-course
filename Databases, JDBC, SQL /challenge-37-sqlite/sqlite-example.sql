8d1a0cc6f9bb03f952d6a53c257018dda005b484
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-01-25 23:09:35 +0300

    solution to challenge 37: more challenges (part 2)

--- Databases, JDBC, SQL /challenge-37-sqlite/sqlite-example.sql
+++ Databases, JDBC, SQL /challenge-37-sqlite/sqlite-example.sql
@@ -42,3 +42,108 @@ SELECT songs.track, songs.title, albums.name
 FROM songs INNER
   JOIN albums ON songs.album = albums._id
 ORDER BY albums.name, songs.track;
+
+
+-- challenge - produce a list of all the artists with their albums in alphabetical order of artists name
+
+SELECT artists.name, albums.name
+FROM albums INNER
+  JOIN artists ON albums.artist = artists._id
+ORDER BY artists.name;
+
+
+-- example - adding view
+
+CREATE VIEW artist_list AS
+  SELECT artists.name AS artist, albums.name AS album, songs.track, songs.title
+  FROM songs INNER
+    JOIN albums ON songs.album = albums._id
+    INNER JOIN artists ON albums.artist = artists._id
+  ORDER BY artists.name
+  , albums.name
+  , songs.track;
+
+
+-- challenge - select the titles of all the songs on the album "Forbidden"
+
+SELECT songs.title
+FROM songs INNER
+  JOIN albums ON songs.album = albums._id
+WHERE albums.name = "Forbidden";
+
+
+-- challenge - repeat the previous query but this time display the songs
+--             in track order now you may want to include the track number in the output
+
+SELECT songs.track, songs.title
+FROM songs INNER
+  JOIN albums ON songs.album = albums._id
+WHERE albums.name = "Forbidden"
+ORDER BY songs.track;
+
+
+-- challenge - display all songs for the band "Deep Purple"
+
+SELECT artists.name, songs.title
+FROM songs INNER
+  JOIN albums ON songs.album = albums._id
+  INNER JOIN artists ON albums.artist = artists._id
+WHERE artists.name = "Deep Purple"
+ORDER BY songs.track;
+
+SELECT *
+FROM artist_list
+WHERE artist = "Deep Purple";
+
+
+-- challenge - rename the band "Mehitable" to "One kitten"
+
+UPDATE artists SET name = 'One kitten' WHERE artists.name = 'Mehitabel';
+
+
+-- challenge - select the title of all the songs by "Aerosmith"
+--             in alphabetical order include only the title in the output
+
+SELECT songs.title
+FROM songs INNER
+  JOIN albums ON songs.album = albums._id
+  INNER JOIN artists ON albums.artist = artists._id
+WHERE artists.name = "Aerosmith"
+ORDER BY songs.title;
+
+SELECT title
+FROM artist_list
+WHERE artist = "Aerosmith"
+ORDER BY title;
+
+
+-- challenge - replace the column that you used in the previous answer with count title in parentheses
+--             to get just a count of the number of the songs number of songs
+
+SELECT COUNT(title)
+FROM artist_list
+WHERE artist = "Aerosmith";
+
+
+-- challenge - select the title of all the songs by "Aerosmith" without duplicates
+
+SELECT DISTINCT title
+FROM artist_list
+WHERE artist = "Aerosmith"
+ORDER BY title;
+
+-- challenge - find out how to get a count of the songs without duplicates
+
+SELECT COUNT(DISTINCT title)
+FROM artist_list
+WHERE artist = "Aerosmith";
+
+-- challenge - repeat the previous query to find the number of artists and the number albums
+
+SELECT COUNT(DISTINCT artist)
+FROM artist_list
+WHERE artist = "Aerosmith";
+
+SELECT COUNT(DISTINCT album)
+FROM artist_list
+WHERE artist = "Aerosmith";
