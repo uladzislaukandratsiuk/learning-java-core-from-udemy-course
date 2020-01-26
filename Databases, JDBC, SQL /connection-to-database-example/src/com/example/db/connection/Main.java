54bbbc3aa05b543a2b739ea37cdba17dbeb975b7
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-01-26 22:01:01 +0300

    JDBC CRUD(create, read, update, delete) example

--- Databases, JDBC, SQL /connection-to-database-example/src/com/example/db/connection/Main.java
+++ Databases, JDBC, SQL /connection-to-database-example/src/com/example/db/connection/Main.java
@@ -8,15 +8,35 @@ public class Main {
             = "jdbc:sqlite:/home/vlad/java-core/Databases, JDBC, SQL /connection-to-database-example/test.db";
 
     private static final String CREATE_TABLE_CONTACTS
-            = "CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)";
+            = "CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)";
 
     public static void main(String[] args) {
 
         try (Connection myConn = DriverManager.getConnection(URL);
              Statement myStmt = myConn.createStatement()) {
 
-            myStmt.executeQuery(CREATE_TABLE_CONTACTS);
+            myStmt.execute(CREATE_TABLE_CONTACTS);
 
+            myStmt.execute("INSERT INTO contacts (name, phone, email) " +
+                    "VALUES  ('Vlad', 1425232, 'vlad@mail.com')");
+            myStmt.execute("INSERT INTO contacts (name, phone, email) " +
+                    "VALUES  ('Ivan', 7451232, 'ivan@mail.com')");
+            myStmt.execute("INSERT INTO contacts (name, phone, email) " +
+                    "VALUES  ('Ann', 2371243, 'ann@mail.com')");
+
+            myStmt.execute("UPDATE contacts SET phone=5217451 WHERE name='Ann'");
+
+            myStmt.execute("DELETE FROM contacts WHERE name='Vlad'");
+
+            myStmt.execute("SELECT * FROM contacts");
+
+            try (ResultSet myRs = myStmt.getResultSet()) {
+                while (myRs.next()) {
+                    System.out.println(myRs.getString("name") + " "
+                            + myRs.getInt("phone") + " "
+                            + myRs.getString("email"));
+                }
+            }
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());
         }
