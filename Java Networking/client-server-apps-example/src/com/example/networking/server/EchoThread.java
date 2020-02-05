c5c43e80dd3af8d4bc493b0c8ef8902ff39b7db2
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-02-05 19:50:57 +0300

    multi-threading server example

---
+++ Java Networking/client-server-apps-example/src/com/example/networking/server/EchoThread.java
@@ -0,0 +1,36 @@
+package com.example.networking.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class EchoThread extends Thread {
+
+    private Socket socket;
+
+    public EchoThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            while (true) {
+                String echoString = input.readLine();
+                System.out.println("Received client input: " + echoString);
+                if (echoString.equals("exit")) {
+                    break;
+                }
+                output.println("Echo from server: " + echoString);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException " + e.getMessage());
+        }
+    }
+}
--- Java Networking/client-server-apps-example/src/com/example/networking/server/MyServer.java
+++ Java Networking/client-server-apps-example/src/com/example/networking/server/MyServer.java
@@ -1,32 +1,15 @@
 package com.example.networking.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class MyServer {
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(5000)) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Client Connected");
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
             while (true) {
-                String echoString = input.readLine();
-                if (echoString.equals("exit")) {
-                    break;
-                }
-                output.println("Echo from server: " + echoString);
+                new EchoThread(serverSocket.accept()).start();
             }
-
-            System.out.println("Client Disconnected");
-
         } catch (IOException e) {
             System.out.println("Server exception " + e.getMessage());
         }
