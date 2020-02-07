ea04496f9bab80833d07b874e0a4d742cc1bb079
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-02-07 20:32:56 +0300

    high-level api HTTP URL connection example

---
+++ Java Networking/high-level-apis-example/src/com/example/apis/http_url/Main.java
@@ -0,0 +1,46 @@
+package com.example.apis.http_url;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://example.org");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
+            connection.setReadTimeout(10000);
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+
+            if (responseCode != 200) {
+                System.out.println("Error reading web page");
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String line;
+
+            while ((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            inputReader.close();
+
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
+        } catch (
+                IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+}
